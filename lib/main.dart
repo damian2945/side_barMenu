3dbeba630dea944f57d8c182250574995621963a
damian2945 <gdamian0@ucol.mx>
2025-10-28 08:24:58 -0600

    creacion

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,8 @@ class home extends StatelessWidget {
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.only(top: 15.0, bottom: 10.0),
-          children: const <Widget>[
-            DrawerHeader(
+          children: <Widget>[
+            const DrawerHeader(
               decoration: BoxDecoration(
                 color: Color.fromARGB(255, 213, 201, 28),
                 image: DecorationImage(image: AssetImage('Assets/hola.jpg'), fit: BoxFit.cover),
@@ -43,20 +43,49 @@ class home extends StatelessWidget {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Principal())
+                );
+              },
             ),
             ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
+              leading: const Icon(Icons.message),
+              title: const Text('Messages'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Mensajes())
+                );
+              },
             ),
             ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
+              leading: const Icon(Icons.account_circle),
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Perfil())
+                );
+              },
             ),
             ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
+              leading: const Icon(Icons.notifications),
+              title: const Text('Notifications'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Notificaciones())
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Settings())
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {},
             ),
           ],
         ),
@@ -66,6 +95,86 @@ class home extends StatelessWidget {
       ),
       body: const Center(
         child: Text('Hello World'),
+      ),
+    );
+  }
+}
+
+class   Settings extends StatelessWidget {
+  const   Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Settings Page'),
+      ),
+    );
+  }
+}
+
+class   Perfil extends StatelessWidget {
+  const   Perfil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Perfil'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Perfil Page'),
+      ),
+    );
+  }
+}
+
+class   Notificaciones extends StatelessWidget {
+  const   Notificaciones({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notificaciones'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Notificaciones Page'),
+      ),
+    );
+  }
+}
+
+class   Mensajes extends StatelessWidget {
+  const Mensajes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mensajes'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Mensajes Page'),
+      ),
+    );
+  }
+}
+
+class Principal extends StatelessWidget {
+  const Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Principal'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Principal Page'),
       ),
     );
   }
